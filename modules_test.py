32ae4563eb2bfe2ecb16616baa1217b547498b73
EuikyunKo <eko004@fiu.edu>
2025-02-27 20:44:33 -0500

    Update modules_test.py
    Edited class TestDisplayActivitySummary(unittest.TestCase)

--- modules_test.py
+++ modules_test.py
@@ -21,11 +21,26 @@ class TestDisplayPost(unittest.TestCase):
 
 
 class TestDisplayActivitySummary(unittest.TestCase):
-    """Tests the display_activity_summary function."""
+    """
+    Tests the display_activity_summary function
 
+    If workouts_list = ['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400]
+    then, test_logic(workouts_list) returns ['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400]
+    thus, list = ['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400]
+    
+    It's hard to perform Unit Testing in Streamlit components like st.write() or st.button()
+    So, test_logic function can be tested independently from the Streamlit components 
+    to indirectly make sure input data and output data match 
+    
+    """
     def test_foo(self):
-        """Tests foo."""
-        pass
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[0], '1:31:29')
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[1], '3:46:38')
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[2], 4)
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[3], (25.745178, -80.366124))
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[4], (25.728228, -80.270986))
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[5], 15000)
+        self.assertEqual(test_logic(['1:31:29', '3:46:38', 4, (25.745178, -80.366124), (25.728228, -80.270986), 15000, 400])[6], 400)
 
 
 class TestDisplayGenAiAdvice(unittest.TestCase):
