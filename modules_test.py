c597f55dbffe41a4493255f80fc2cdab13fbd281
alaincodex <156149099+alaincodex@users.noreply.github.com>
2025-03-02 10:16:46 -0500

    Update modules_test.py

--- modules_test.py
+++ modules_test.py
@@ -7,7 +7,7 @@
 
 import unittest
 from streamlit.testing.v1 import AppTest
-from modules import display_activity_summary, _validate_workouts
+from modules import display_activity_summary, validate_workouts, display_post, display_recent_workouts, display_genai_advice
 from unittest.mock import patch
 import re
 
@@ -33,36 +33,33 @@ class TestDisplayActivitySummary(unittest.TestCase):
                 "distance": 10.0,
                 "steps": 10000,
                 "calories_burned": 50,
-            },
-            {
-                "workout_id": "workout_2",
-                "start_timestamp": "2024-02-01 00:00:00",
-                "end_timestamp": "2024-02-01 00:30:00",
-                "start_lat_lng": (1.11, 4.22),
-                "end_lat_lng": (2.22, 4.44),
-                "distance": 5.0,
-                "steps": 1000,
-                "calories_burned": 10,
-            },
+            }
         ]
 
         display_activity_summary(workouts_list)
 
-        # Check expected function calls
         mock_write.assert_any_call("- Start Time: 2024-01-01 00:10:00")
-        mock_write.assert_any_call("- Distance: 10.0 km")  # Fixed format
+        mock_write.assert_any_call("- Distance: 10.0 km")
         mock_write.assert_any_call("- Steps: 10000")
-        mock_write.assert_any_call("- Calories burned: 50")
 
     def test_validate_workouts(self):
-        """Test _validate_workouts function returns input data correctly"""
+        """Test validate_workouts function returns input data correctly"""
         workouts_list = [
             {"workout_id": "workout_1", "distance": 10.0},
             {"workout_id": "workout_2", "distance": 5.0},
         ]
-        self.assertEqual(_validate_workouts(workouts_list), workouts_list)
+        self.assertEqual(validate_workouts(workouts_list), workouts_list)
+
+class TestDisplayPost(unittest.TestCase):
+    """Tests the display_post function."""
+
+    @patch("streamlit.write")
+    def test_display_post(self, mock_write):
+        """Ensures the display_post function outputs expected text."""
+        display_post("John Doe", "user_img.jpg", "2024-01-01", "This is a post!", None)
+
+        mock_write.assert_any_call("📅 2024-01-01")
+        mock_write.assert_any_call("This is a post!")
 
 if __name__ == "__main__":
     unittest.main()
-
-
