6745d820255367de309552e2caf02884176e5f3c
ebrletic <brleticeric@gmail.com>
2025-03-21 15:38:22 +0000

    Fixed modules_test.py to use mock data instead of actual get_* functions

--- modules.py
+++ modules.py
@@ -7,28 +7,9 @@
 # function other than the example.
 #############################################################################
 
-from internals import create_component
 import streamlit as st
 import pandas as pd
 import altair as alt
-
-# This one has been written for you as an example. You may change it as wanted.
-def display_my_custom_component(value):
-    """Displays a 'my custom component' which showcases an example of how custom
-    components work.
-
-    value: the name you'd like to be called by within the app
-    """
-    # Define any templated data from your HTML file. The contents of
-    # 'value' will be inserted to the templated HTML file wherever '{{NAME}}'
-    # occurs. You can add as many variables as you want.
-    data = {
-        'NAME': value,
-    }
-    # Register and display the component by providing the data and name
-    # of the HTML file. HTML must be placed inside the "custom_components" folder.
-    html_file_name = "my_custom_component"
-    create_component(data, html_file_name)
 
 
 def display_post(username, user_image, timestamp, content, post_image):
@@ -144,8 +125,8 @@ def display_genai_advice(timestamp, content, image):
         <div class="genai-advice">
             <h2>GenAI Advice</h2>
             {f'<p>{content}</p>' if content else '<p>No advice today :(</p>'}
-            {f'<p><em>{timestamp}</em></p>' if timestamp else ''}
-            {'<img src="' + image + '" width="200" />' if image else ''}
+            {f'<p><em>{timestamp}</em></p>' if timestamp else '<p></p>'}
+            {'<img src="' + image + '" width="200" />' if image else '<p></p>'}
         """,
         unsafe_allow_html=True
     )
