277492b00a0e89ce0ae9b26b1eb93a0ef62d6c44
ebrletic <brleticeric@gmail.com>
2025-03-21 21:35:31 +0000

    Fixed random import again :(

--- community_page.py
+++ community_page.py
@@ -2,6 +2,7 @@ import streamlit as st
 from home_page import userId
 from modules import display_genai_advice, display_post
 from data_fetcher import get_genai_advice, get_user_profile, get_user_posts, users
+import random
 userId = random.choice(list(users.keys()))
 
 st.title('Community')
